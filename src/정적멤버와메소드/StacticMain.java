bd572a3d5c9e900884a26a092474e5e6ba35a8d8
Eyelid <lee940706@gmail.com>
2023-08-24 17:49:21 +0900

    first commit -

---
+++ src/정적멤버와메소드/StacticMain.java
@@ -0,0 +1,26 @@
+package 정적멤버와메소드;
+
+import java.security.Key;
+
+public class StacticMain {
+    public static void main(String[] args) {
+
+
+    Bank kakao = new Bank("카카오", 1000);
+    Bank shinhan = new Bank("신한", 1000);
+    Bank nj = new Bank("농협", 0);
+
+    kakao.setDeposit(2300);
+    kakao.setwhithDraw(1850);
+    kakao.viewAccount();
+    shinhan.setwhithDraw(2000);
+    shinhan.viewAccount();
+
+
+    System.out.println(Bank.getCount()); //클래스에 대한 메소드 접근.
+
+}
+}
+
+
+
